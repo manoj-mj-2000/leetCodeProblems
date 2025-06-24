5e903aa3d55f3f23165b773072ef79e9f231bc2d
Manoj <113231045+manoj-mj-2000@users.noreply.github.com>
2025-06-24 13:49:14 +0530

    IsAnagram Leetcode problem

---
+++ IsAnagram.java
@@ -0,0 +1,35 @@
+//$Id$
+package com.leet;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+public class IsAnagram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "car";
+		String t = "rac";
+		
+		System.out.println(isAnagram(s,t));
+	}
+	
+	public static boolean isAnagram(String s, String t) {
+		
+		if(s.length() != t.length())
+			return false;
+		int count[] = new int[26];
+		for(int i=0;i<s.length(); i++) {
+			count[s.charAt(i) - 'a']++;
+			count[t.charAt(i) - 'a']--;
+		}
+		
+		for(int i=0;i<count.length;i++) {
+			if(count[i] != 0)
+				return false;
+		}
+		return true;
+		
+	}
+
+}
